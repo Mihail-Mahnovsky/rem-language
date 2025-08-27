353757b40b7e114e6d1db78c23c86ea8a5a78c4c
Mako <mihailevgenovich@gmail.com>
2025-08-27 04:22:01 -0700

    mini binOp refactor

--- source/parser/ast/BinaryNode.cpp
+++ source/parser/ast/BinaryNode.cpp
@@ -8,90 +8,97 @@
 
 BinaryNode::BinaryNode(Node *left, Node *right, Operations operation) :left(left),right(right),operation(operation){}
 
+std::any BinaryNode::evalDouble(std::any rightVal,std::any leftVal)
+{
+    double l = std::any_cast<double>(leftVal);
+    double r = std::any_cast<double>(rightVal);
+
+    switch (operation) {
+    case Operations::PLUS:
+        return l + r;
+    case Operations::MINUS:
+        return l - r;
+    case Operations::MULTIPLY:
+        return l * r;
+    case Operations::DIVISION:
+        if (r == 0) throw std::runtime_error("Division by zero");
+        return l / r;
+    case Operations::EQUAL:
+        return l == r;
+    case Operations::NOTEQUAL:
+        return l != r;
+    case Operations::MIN:
+        return l < r;
+    case Operations::MAX:
+        return l > r;
+    case Operations::MINEQUAL:
+        return l <= r;
+    case Operations::MAXEQUAL:
+        return l >= r;
+    default:
+        throw std::runtime_error("Unknown operation");
+    }
+}
+
+std::any BinaryNode::evalBool(std::any rightVal, std::any leftVal){
+    bool l = std::any_cast<bool>(leftVal);
+    bool r = std::any_cast<bool>(rightVal);
+
+    switch (operation) {
+    case Operations::AND:
+        return l && r;
+    case Operations::OR:
+        return l || r;
+    case Operations::EQUAL:
+        return l == r;
+    case Operations::NOTEQUAL:
+        return l != r;
+    default:
+        throw std::runtime_error("Unsupported boolean operation");
+    }
+}
+
+std::any BinaryNode::evalString(std::any rightVal, std::any leftVal){
+    std::string l = std::any_cast<std::string>(leftVal);
+    std::string r = std::any_cast<std::string>(rightVal);
+
+    switch (operation) {
+    case Operations::PLUS:
+        return l + r;
+    case Operations::EQUAL:
+        return l == r;
+    case Operations::NOTEQUAL:
+        return l != r;
+    default:
+        throw std::runtime_error("Unknown operation for string");
+    }
+}
+
+std::any BinaryNode::evalChar(std::any rightVal, std::any leftVal){
+    std::string l = (leftVal.type() == typeid(char) && rightVal.type() == typeid(std::string))
+                ? std::string(1, std::any_cast<char>(leftVal))
+                : std::any_cast<std::string>(leftVal);
+    std::string r = (rightVal.type() == typeid(char))
+                    ? std::string(1, std::any_cast<char>(rightVal))
+                    : std::any_cast<std::string>(rightVal);
+    switch (operation) {
+    case Operations::PLUS:
+        return l + r;
+    default:
+        throw std::runtime_error("Unknown operation for string and char");
+    }
+}
+
+
+
 std::any BinaryNode::evaluate(Context& context) {
     std::any leftVal = left->evaluate( context);
     std::any rightVal = right->evaluate(context);
 
-    if (leftVal.type() == typeid(double) && rightVal.type() == typeid(double)) {
-        double l = std::any_cast<double>(leftVal);
-        double r = std::any_cast<double>(rightVal);
-
-        switch (operation) {
-            case Operations::PLUS:
-                return l + r;
-            case Operations::MINUS:
-                return l - r;
-            case Operations::MULTIPLY:
-                return l * r;
-            case Operations::DIVISION:
-                if (r == 0) throw std::runtime_error("Division by zero");
-                return l / r;
-            case Operations::EQUAL:
-                return l == r;
-            case Operations::NOTEQUAL:
-                return l != r;
-            case Operations::MIN:
-                return l < r;
-            case Operations::MAX:
-                return l > r;
-            case Operations::MINEQUAL:
-                return l <= r;
-            case Operations::MAXEQUAL:
-                return l >= r;
-            default:
-                throw std::runtime_error("Unknown operation");
-        }
-    }
-
-    else if (leftVal.type() == typeid(bool) && rightVal.type() == typeid(bool)) {
-        bool l = std::any_cast<bool>(leftVal);
-        bool r = std::any_cast<bool>(rightVal);
-
-        switch (operation) {
-        case Operations::AND:
-            return l && r;
-        case Operations::OR:
-            return l || r;
-        case Operations::EQUAL:
-            return l == r;
-        case Operations::NOTEQUAL:
-            return l != r;
-        default:
-            throw std::runtime_error("Unsupported boolean operation");
-        }
-    }
-
-
-    else if (leftVal.type() == typeid(std::string) && rightVal.type() == typeid(std::string)) {
-        std::string l = std::any_cast<std::string>(leftVal);
-        std::string r = std::any_cast<std::string>(rightVal);
-
-        switch (operation) {
-            case Operations::PLUS:
-                return l + r;
-            case Operations::EQUAL:
-                return l == r;
-            case Operations::NOTEQUAL:
-                return l != r;
-            default:
-                throw std::runtime_error("Unknown operation for string");
-        }
-    }
-
-    else if ((leftVal.type() == typeid(char) && rightVal.type() == typeid(std::string)) || (leftVal.type() == typeid(std::string) && rightVal.type() == typeid(char))) {
-        std::string l = (leftVal.type() == typeid(char) && rightVal.type() == typeid(std::string))
-                        ? std::string(1, std::any_cast<char>(leftVal))
-                        : std::any_cast<std::string>(leftVal);
-        std::string r = (rightVal.type() == typeid(char))
-                        ? std::string(1, std::any_cast<char>(rightVal))
-                        : std::any_cast<std::string>(rightVal);
-        switch (operation) {
-            case Operations::PLUS:
-                return l + r;
-            default:
-                throw std::runtime_error("Unknown operation for string and char");
-        }
-    }
+    if (leftVal.type() == typeid(double) && rightVal.type() == typeid(double)) {return evalDouble(leftVal,rightVal);}
+    else if (leftVal.type() == typeid(bool) && rightVal.type() == typeid(bool)) { return evalBool(leftVal,rightVal); }
+    else if (leftVal.type() == typeid(std::string) && rightVal.type() == typeid(std::string)) {return evalString(leftVal,rightVal);}
+    else if ((leftVal.type() == typeid(char) && rightVal.type() == typeid(std::string)) || (leftVal.type() == typeid(std::string) && rightVal.type() == typeid(char))) {return evalChar(leftVal,rightVal);}
 
     //тут просто через елз иф пишешь другие реальзацы(строки чары и тд)
 
--- source/parser/ast/BinaryNode.hpp
+++ source/parser/ast/BinaryNode.hpp
@@ -1,6 +1,8 @@
 #ifndef REMLANG_BINARYNODE_HPP
 #define REMLANG_BINARYNODE_HPP
 
+#include <string>
+
 #include "Operations.hpp"
 #include "Node.hpp"
 
@@ -13,6 +15,10 @@ private:
     Node* left;
     Node* right;
     Operations operation;
+    std::any evalDouble(std::any rightVal,std::any leftVal);
+    std::any evalBool(std::any rightVal,std::any leftVal);
+    std::any evalString(std::any rightVal, std::any leftVal);
+    std::any evalChar(std::any rightVal,std::any leftVal);
 public:
     BinaryNode(Node* left, Node* right, Operations operation);
     std::any evaluate(Context& context) override;
